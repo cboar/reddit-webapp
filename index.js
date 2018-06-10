f8b7adae0e94f9ae8a13df6d6a70b17077e41511
cboar <admin@cboar.org>
2018-06-09 17:53:54 -0700

    Grabs reddit API data on page request

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+const request = require("request")
 const express = require("express")
 const pug = require("pug")
 const app = express()
 
 const index = pug.compileFile("index.pug")
 
-app.get("/", function(req, res){
+app.get("/", function(req, output){
 
-	const out = index()
-	res.send(out)
+	const url = "https://reddit.com/hot.json?limit=25"
+	request(url, function(err, res, body){
+
+		const json = JSON.parse(body)
+		const html = index()
+		output.send(html)
+
+	})
 
 })
 app.listen(8080)

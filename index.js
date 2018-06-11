f40691eaf5053eab0255c602d94712b65c511d0f
cboar <admin@cboar.org>
2018-06-10 21:16:29 -0700

    Added test data and score formatting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const request = require("request")
 const express = require("express")
 const pug = require("pug")
+const fs = require("fs")
 const app = express()
 
 const index = pug.compileFile("index.pug")
@@ -8,12 +9,18 @@ const index = pug.compileFile("index.pug")
 app.use(express.static("public"))
 app.get("/", function(req, output){
 
-	const url = "https://reddit.com/hot.json?limit=10"
-	request(url, function(err, res, body){
+	const url = "https://reddit.com/hot.json?limit=50"
+	fs.readFile("testdata", { encoding: "utf8" }, function(err, body){
+	//request(url, function(err, res, body){
 
 		const data = JSON.parse(body).data
-		const items = data.children.map(a => a.data)
-		const html = index({ items: items })
+		const items = data.children.map(function(a){
+			const score = a.data.score
+			if(score > 999)
+				a.data.score = (score / 1000).toFixed(1) + "K"
+			return a.data
+		})
+		const html = index({ offset: 0, items: items })
 		output.send(html)
 
 	})
